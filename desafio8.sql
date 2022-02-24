ec25434cdf1497fd7a79ed5d6930dfb898c289ec
tonistorres <sisvenda2011@gmail.com>
2022-02-24 17:40:15 -0300

    feat:Implementando Qt08

---
+++ desafio8.sql
@@ -0,0 +1,3 @@
+#8 - Faça uma consulta que retorne três colunas, respectivamente, com os nomes 'A', 'Trybe' e 'eh', e com valores referentes 
+# a soma de '5 + 6', a string 'de', a soma de '2 + 8'.
+SELECT 5 + 6 AS A, 'de' AS 'Trybe', 2 + 8 AS 'eh';
